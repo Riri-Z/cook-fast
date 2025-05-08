4f9aff2bdb11ec726be668426ac605d67bb2b8aa
Riri-Z <riri.zamor@gmail.com>
2025-05-08 12:11:45 +0200

    Feat : add daisy , and toggleTheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { FoodProvider } from "@/components/food-provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <FoodProvider>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen flex items-center justify-center `}>
-          {children}
+        <body className="antialiased w-screen h-screen flex items-center justify-center">
+          <ThemeProvider>{children}</ThemeProvider>
         </body>
       </FoodProvider>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
+import ToggleTheme from "@/components/toggle-theme";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-red-900 h-full w-full">
+    <div className="h-full w-full">
       <p>Test</p>
       <Link href="/dashboard">dashboaard</Link>
+      <ToggleTheme />
     </div>
   );
 }

cd8b4f41db04a94656896816703f5fe96e191dc0
Riri-Z <riri.zamor@gmail.com>
2025-06-02 13:47:58 +0200

    Feat : add scroll on tags container

--- src/app/ui/Recipe/tags-recipe.tsx
+++ src/app/ui/Recipe/tags-recipe.tsx
@@ -1,36 +1,64 @@
 import clsx from 'clsx';
+import { ChevronLeftCircle, ChevronRightCircle } from 'lucide-react'; // ou autre icône
+import { useState, useRef, useEffect } from 'react';
 
 type TagsRecipeProps = {
   tags: string[];
   variant?: string;
 };
 
+const SCROLL_VALUE = 100;
+
 export default function TagsRecipe({
   tags,
   variant = 'badge-success',
 }: Readonly<TagsRecipeProps>) {
-  const Container = ({ tags }: { tags: string[] }) => {
-    /* 
-        -  sort pour afficher le plus court
-        - si plus de trois, 
-          j'affiche 2 => et un dernier on on affiche '3+ ' par exemple 
-        - sinon on affiche les trois 
-      
-      */
-    let sortedTags = tags.sort((a, b) => a.length - b.length);
-    const tagslength = sortedTags.length;
-    console.log('tagslength', tagslength);
-    if (tagslength > 3) {
-      sortedTags = sortedTags.slice(0, 2);
-      sortedTags[2] = tagslength - 2 + '+';
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [atStart, setAtStart] = useState<null | boolean>(true);
+  const [atEnd, setAtEnd] = useState<null | boolean>(false);
+  // Handle scroll behaviour
+  const scroll = (direction: 'left' | 'right') => {
+    if (!scrollRef.current) return;
+    scrollRef.current.scrollBy({
+      left: direction === 'left' ? -SCROLL_VALUE : SCROLL_VALUE,
+      behavior: 'smooth',
+    });
+  };
+
+  const checkOverflow = () => {
+    const el = scrollRef.current;
+    if (el) {
+      setAtStart(el.scrollLeft <= 1);
+
+      setAtEnd(el.scrollLeft + el.clientWidth >= el.scrollWidth - 1);
     }
-    console.log('sortedTags', sortedTags);
+  };
+
+  useEffect(() => {
+    checkOverflow();
+    const el = scrollRef.current;
+    if (!el) return;
+    window.addEventListener('resize', checkOverflow);
+
+    // Listen to the scroll event because smooth scrolling updates scrollLeft asynchronously
+    el.addEventListener('scroll', checkOverflow);
+    return () => {
+      window.removeEventListener('resize', checkOverflow);
+      el.removeEventListener('scroll', checkOverflow);
+    };
+  }, []);
+
+  const Container = ({ tags }: { tags: string[] }) => {
+    const sortedTags = tags.toSorted((a, b) => a.length - b.length);
+
     return (
       <>
         {sortedTags.length > 0 ? (
-          sortedTags.map((tag) => (
-            <Badge key={tag} tag={tag} variant={variant} />
-          ))
+          <main className="flex gap-1">
+            {sortedTags.map((tag) => (
+              <Badge key={tag} tag={tag} variant={variant} />
+            ))}
+          </main>
         ) : (
           <Badge tag="None" variant={variant} />
         )}
@@ -39,8 +67,28 @@ export default function TagsRecipe({
   };
 
   return (
-    <div className="flex flex-wrap gap-1">
-      <Container tags={tags} />
+    <div className="flex items-center justify-between gap-1">
+      {!atStart && (
+        <ChevronLeftCircle
+          className="hover:scale-110"
+          onClick={() => scroll('left')}
+          size={24}
+        />
+      )}
+      <div
+        ref={scrollRef}
+        className="flex gap-2 overflow-hidden"
+        style={{ scrollBehavior: 'smooth' }}
+      >
+        <Container tags={tags} />
+      </div>
+      {!atEnd && (
+        <ChevronRightCircle
+          className="hover:scale-110"
+          onClick={() => scroll('right')}
+          size={24}
+        />
+      )}
     </div>
   );
 }
